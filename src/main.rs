d452b0ed48d29c2dec1227807d428fcaeb81547a
Tyler Moore <TylerRMoore@gmail.com>
2025-08-18 17:50:40 -0700

    Working proof of concept

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,55 @@
-use std::{error::Error, net::{IpAddr, SocketAddr}, thread::sleep, time::Duration};
+use std::{
+    error::Error,
+    net::{IpAddr, SocketAddr},
+    thread::sleep,
+    time::Duration,
+};
 
-use sacn::{packet::ACN_SDT_MULTICAST_PORT, source::SacnSource};
+use ddp_rs::{connection, protocol};
 
-fn main() -> Result<(), Box<dyn Error>> {    
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Starting.");
 
-    let local_addr: SocketAddr = SocketAddr::new(IpAddr::V4("0.0.0.0".parse().unwrap()), ACN_SDT_MULTICAST_PORT + 1);
+    let mut conn = connection::DDPConnection::try_new(
+        "10.10.30.17:4048",               // The IP address of the device followed by :4048
+        protocol::PixelConfig::default(), // Default is RGB, 8 bits ber channel
+        protocol::ID::Default,
+        std::net::UdpSocket::bind("0.0.0.0:6969").unwrap(), // can be any unused port on 0.0.0.0, but protocol recommends 4048
+    )?;
 
-    let mut src = SacnSource::with_ip("Source", local_addr).unwrap();
+    // loop sets some colors for the first 6 pixels to see if it works
+    for i in 0u8..100u8 {
+        let high = 10u8.overflowing_mul(i).0;
 
-    let universe: u16 = 1;
-    let sync_uni: Option<u16> = Some(1); // Data packets use a synchronisation address of 1.
-    let priority: Option<u8> = Some(100); // The priority for the sending data, must be 1-200 inclusive,  None means use default.
-    
-    // To send using unicast the dst_ip argument is set to a Some() value with the address to send the data to. By default the port should be the
-    // ACN_SDT_MULTICAST_PORT but this can be configured differently if required in a specific situation. Change this address to the correct address for your
-    // application, 192.168.0.1 is just a stand-in.
-    let destination_address: SocketAddr = SocketAddr::new(IpAddr::V4("10.10.30.17".parse().unwrap()), ACN_SDT_MULTICAST_PORT);
-    let dst_ip: Option<SocketAddr> = Some(destination_address);
+        // loop through some colors
 
-    src.register_universe(universe).unwrap(); // Register with the source that will be sending on the given universe.
+        let temp: usize = conn.write(&[
+            high, /*red value*/
+            0,    /*green value*/
+            0,    /*blue value*/
+            high, /*red value*/
+            0,    /*green value*/
+            0,    /*blue value*/
+            0,    /*red value*/
+            high, /*green value*/
+            0,    /*blue value*/
+            0,    /*red value*/
+            high, /*green value*/
+            0,    /*blue value*/
+            0,    /*red value*/
+            0,    /*green value*/
+            high, /*blue value*/
+            0,    /*red value*/
+            0,    /*green value*/
+            high, /*blue value*/
+        ])?;
 
-    let mut data: Vec<u8> = vec![0, 0, 0, 0, 255, 255, 128, 128]; // Some arbitrary data, must have length <= 513 (including start-code).
+        std::thread::sleep(std::time::Duration::from_millis(10));
+        // this crate is non blocking, so with out the sleep, it will send them all instantly
 
-    // Actually send the data, since the sync_uni is not 0 the data will be synchronised at the receiver (if the receiver supports synchronisation).
-    src.send(&[universe], &data, priority, dst_ip, sync_uni).unwrap();
+        println!("sent {temp} packets");
+    }
 
-    // A small delay between sending data and sending the sync packet as recommend in ANSI E1.31-2018 Section 11.2.2.
-    sleep(Duration::from_millis(10));
-    
-    // To actually trigger the data need to send a synchronisation packet like so.
-    src.send_sync_packet(sync_uni.unwrap(), dst_ip).unwrap();
-    
+    println!("Finished.");
     Ok(())
 }
